8e161375321b95b879dc0f92613ddfa379218319
ololx <ololx@icloud.com>
2022-12-23 17:26:04 +0200

    chore(*) implement empty tuple

---
+++ moonshine-tuples/src/main/java/io/github/ololx/moonshine/tuples/EmptyTuple.java
@@ -0,0 +1,52 @@
+/**
+ * Copyright 2022 the project moonshine authors
+ * and the original author or authors annotated by {@author}
+ * <p>
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * <p>
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * <p>
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package io.github.ololx.moonshine.tuples;
+
+import java.util.Iterator;
+import java.util.Spliterator;
+import java.util.function.Consumer;
+
+/**
+ * project moonshine
+ * created 23.12.2022 10:34
+ *
+ * @author Alexander A. Kropotin
+ */
+public class EmptyTuple implements NullTuple {
+
+    private static final int size = 0;
+
+    @Override
+    public int size() {
+        return size;
+    }
+
+    @Override
+    public <V> V get(int index) {
+        return null;
+    }
+
+    @Override
+    public <V> V getOrDefault(int index, V defaultValue) {
+        return defaultValue;
+    }
+
+    @Override
+    public Iterator<Object> iterator() {
+        return null;
+    }
+}
