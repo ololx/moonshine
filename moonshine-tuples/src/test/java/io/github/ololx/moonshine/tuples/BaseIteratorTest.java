e2cf78104c7988fa30b64aa624732aa0c93e68bd
a-kropotin <a.kropotin@pixonic.com>
2023-01-05 08:47:57 +0200

    chore(*) change genetic names (code style problem)

--- moonshine-tuples/src/test/java/io/github/ololx/moonshine/tuples/BaseIteratorTest.java
+++ moonshine-tuples/src/test/java/io/github/ololx/moonshine/tuples/BaseIteratorTest.java
@@ -14,6 +14,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
+
 package io.github.ololx.moonshine.tuples;
 
 import org.testng.annotations.DataProvider;
