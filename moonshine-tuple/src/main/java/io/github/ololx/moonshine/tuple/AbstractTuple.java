8c17283951c545c041dd563fee0057023c0eaf1f
ololx <ololx@icloud.com>
2023-01-18 22:34:06 +0200

    chore(*) add octuple tests

---
+++ moonshine-tuple/src/main/java/io/github/ololx/moonshine/tuple/AbstractTuple.java
@@ -0,0 +1,47 @@
+/**
+ * Copyright 2023 the project moonshine authors
+ * and the original author or authors annotated by {@author}
+ * <p>
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * <p>
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * <p>
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package io.github.ololx.moonshine.tuple;
+
+import java.util.Arrays;
+import java.util.stream.IntStream;
+
+/**
+ * project moonshine
+ * created 18.01.2023 18:59
+ * <p>
+ * @author Alexander A. Kropotin
+ */
+abstract class AbstractTuple implements Tuple {
+
+    @Override
+    public String toString() {
+        final int elementsCount = this.size();
+        final int tupleStringLength = elementsCount == 0 ? 2 : elementsCount * 3;
+        final StringBuilder tupleStringBuilder = new StringBuilder(tupleStringLength);
+
+        IntStream.range(0, elementsCount - 1).forEach(index -> {
+            tupleStringBuilder.append(this.get(index));
+            tupleStringBuilder.append(", ");
+        });
+
+        tupleStringBuilder.insert(0, "(");
+        tupleStringBuilder.append(")");
+
+        return tupleStringBuilder.toString();
+    }
+}
