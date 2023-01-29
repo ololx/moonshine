2b01b47d4bb1ef08d43a0bba5d8c110e00bc8b71
ololx <ololx@icloud.com>
2023-01-29 13:15:54 +0200

    feature(TupleString.java) implement utility methods for the building tuple strimg representation

---
+++ moonshine-tuple/src/main/java/io/github/ololx/moonshine/tuple/TupleString.java
@@ -0,0 +1,73 @@
+/**
+ * Copyright 2023 the project moonshine authors
+ * and the original author or authors annotated by {@author}
+ * <p>
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * <p>
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * <p>
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package io.github.ololx.moonshine.tuple;
+
+import java.util.stream.IntStream;
+
+/**
+ * An interpreter for the string representation of tuples. This class provides
+ * utility methods for creating print-style string representations of a specific
+ * tuple.
+ *
+ * <p>These methods are designed primarily for the building string from tuple
+ * in methods and constructors, as demonstrated below:
+ * <blockquote><pre>
+ * public void printTuple(Tuple tuple) {
+ *      System.out.println(TupleString.format(tuple));
+ * }
+ * </pre></blockquote>
+ *
+ * project moonshine
+ * created 29.01.2023 12:34
+ * <p>
+ * @author Alexander A. Kropotin
+ */
+final class TupleString {
+
+    /**
+     * Override constructor by defaults (implicit public constructor).
+     * Because utility class are not meant to be instantiated.
+     */
+    private TupleString() {}
+
+    /**
+     * Returns a formatted string using the tuple argument.
+     *
+     * @param tuple the specified tuple
+     * @return the formatted string of the specified tuple. Typically,
+     * a formatted string contains a sequence of the elements of a tuple
+     * in parentheses.
+     * @see  AbstractTuple#toString()
+     */
+    static String format(Tuple tuple) {
+        final int elementsCount = tuple.size();
+        final int tupleStringLength = elementsCount == 0 ? 2 : elementsCount * 3;
+        final StringBuilder tupleStringBuilder = new StringBuilder(tupleStringLength);
+
+        tupleStringBuilder.append(String.valueOf(tuple.getOrDefault(0, "")));
+        IntStream.range(1, elementsCount).forEach(index -> {
+            tupleStringBuilder.append(", ");
+            tupleStringBuilder.append(String.valueOf(tuple.getOrDefault(index, "")));
+        });
+
+        tupleStringBuilder.insert(0, "(");
+        tupleStringBuilder.append(")");
+
+        return tupleStringBuilder.toString();
+    }
+}
