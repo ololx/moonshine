a1894050806ecb2450db812bf4d2a3949b76a73d
ololx <ololx@icloud.com>
2023-01-30 21:58:28 +0200

    chore(*) add generics to general get methods other tuples

--- moonshine-tuple/src/main/java/io/github/ololx/moonshine/tuple/Quintuple.java
+++ moonshine-tuple/src/main/java/io/github/ololx/moonshine/tuple/Quintuple.java
@@ -112,19 +112,20 @@ public class Quintuple<A, B, C, D, E>
      * @throws IndexOutOfBoundsException if the index is out of
      * range ({@code index < 0 || index >= size()})
      */
+    @SuppressWarnings("unchecked")
     @Override
-    public final Object get(int index) {
+    public final <V> V get(int index) {
         switch (IndexBounds.requireIndexWithinBounds(index, this.size())) {
             case 0:
-                return this.t0;
+                return (V) this.t0;
             case 1:
-                return this.t1;
+                return (V) this.t1;
             case 2:
-                return this.t2;
+                return (V) this.t2;
             case 3:
-                return this.t3;
+                return (V) this.t3;
             default:
-                return this.t4;
+                return (V) this.t4;
         }
     }
 
--- moonshine-tuple/src/main/java/io/github/ololx/moonshine/tuple/Tuple.java
+++ moonshine-tuple/src/main/java/io/github/ololx/moonshine/tuple/Tuple.java
@@ -83,7 +83,7 @@ public interface Tuple extends Iterable<Object> {
      * @throws IndexOutOfBoundsException if the index is out of
      * range ({@code index < 0 || index >= size()})
      */
-    <E> E get(int index);
+    <V> V get(int index);
 
     /**
      * Returns the element at the specified position in this tuple, or
@@ -101,12 +101,13 @@ public interface Tuple extends Iterable<Object> {
      * @throws ClassCastException if the tuple element is of an inappropriate
      * type for this {@code defaultValue}
      */
-    default <E> E getOrDefault(int index, E defaultValue) {
+    @SuppressWarnings("unchecked")
+    default <V> V getOrDefault(int index, V defaultValue) {
         if (!IndexBounds.checkIndex(index, this.size())) {
             return defaultValue;
         }
 
-        return (E) get(index);
+        return (V) get(index);
     }
 
     /**
