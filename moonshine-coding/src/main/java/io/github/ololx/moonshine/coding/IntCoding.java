c885b6e6add45225bb84831ce5fd2dd8be8113b8
a-kropotin <a.kropotin@pixonic.com>
2023-02-10 22:52:36 +0200

    chore(*) init new project

---
+++ moonshine-coding/src/main/java/io/github/ololx/moonshine/coding/IntCoding.java
@@ -0,0 +1,38 @@
+/**
+ * Copyright 2023 the project moonshine authors
+ * and the original author or authors annotated by {@author}
+ * <p>
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * <p>
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * <p>
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package io.github.ololx.moonshine.coding;
+
+/**
+ * project moonshine
+ * created 10.02.2023 13:45
+ *
+ * @author Alexander A. Kropotin
+ */
+public final class IntCoding {
+
+    private IntCoding() {}
+
+    static byte[] encode(int value) {
+        return new byte[] {
+                (byte)(value >>> 24),
+                (byte)(value >>> 16),
+                (byte)(value >>> 8),
+                (byte)(value)
+        };
+    }
+}
