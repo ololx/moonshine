998266ca1a3c4fd0903c44e0c2d27eb86afe496c
a-kropotin <a.kropotin@pixonic.com>
2023-03-16 21:13:39 +0200

    chore(bytes/ByteOrder.java) add javadoc

--- moonshine-bytes/src/main/java/io/github/ololx/moonshine/bytes/ByteOrder.java
+++ moonshine-bytes/src/main/java/io/github/ololx/moonshine/bytes/ByteOrder.java
@@ -21,6 +21,15 @@ import java.util.Objects;
 import java.util.concurrent.atomic.AtomicInteger;
 
 /**
+ * A class that represents a byte order, with a name, unique ID, and a provider
+ * for byte order array.
+ *
+ * @implNote <p>This class is immutable and thread-safe.</p>
+ * <p>This class provides a default implementation of byte order that can be
+ * used as a reference.</p>
+ * <p>The byte order provider can be implemented by the user and provided to
+ * the constructor.</p>
+ *
  * project moonshine
  * created 22.02.2023 14:13
  *
@@ -28,36 +37,81 @@ import java.util.concurrent.atomic.AtomicInteger;
  */
 public class ByteOrder {
 
+    /**
+     * Counter for generating unique IDs.
+     */
     private static final AtomicInteger instancesCount = new AtomicInteger(1);
 
+    /**
+     * The ID of the byte order.
+     */
     private final int id;
 
+    /**
+     * The name of the byte order.
+     */
     private final String name;
 
+    /**
+     * The provider for the byte order array.
+     */
     private final ByteOrderProvider byteOrderProvider;
 
+    /**
+     * Constructs a ByteOrder object with the given name and provider.
+     *
+     * @param name              The name of the byte order.
+     * @param byteOrderProvider The provider for the byte order array.
+     * @throws NullPointerException if the name or provider is null.
+     */
     public ByteOrder(String name, ByteOrderProvider byteOrderProvider) {
         this.name = Objects.requireNonNull(name);
         this.id = instancesCount.incrementAndGet();
         this.byteOrderProvider = Objects.requireNonNull(byteOrderProvider);
     }
 
-    public String getName() {
+    /**
+     * Returns the name of this byte order.
+     *
+     * @return The name of this byte order.
+     */
+    public final String getName() {
         return this.name;
     }
 
-    public int getId() {
+    /**
+     * Returns the ID of this byte order.
+     *
+     * @return The ID of this byte order.
+     */
+    public final int getId() {
         return this.id;
     }
 
-    public int[] byteOrder(int mostSignificantByte) {
+    /**
+     * Returns the byte order array for the given most significant byte.
+     *
+     * @param mostSignificantByte The most significant byte.
+     * @return The byte order array.
+     */
+    public final int[] byteOrder(int mostSignificantByte) {
         return this.byteOrderProvider.provide(mostSignificantByte);
     }
 
-    public ByteOrderProvider getByteOrderProvider() {
+    /**
+     * Returns the byte order provider for this byte order.
+     *
+     * @return The byte order provider for this byte order.
+     */
+    public final ByteOrderProvider getByteOrderProvider() {
         return this.byteOrderProvider;
     }
 
+    /**
+     * Returns a string representation of this byte order.
+     *
+     * @return A string representation of this byte order.
+     */
     @Override
     public String toString() {
         return String.format("%s[%s]", this.name, this.id);
