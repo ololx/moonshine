9b96c7bbc0eda6901260c5777b87eb8a9200f035
a-kropotin <a.kropotin@pixonic.com>
2023-03-22 11:39:25 +0200

    chore(*) add javadoc

--- moonshine-bytes/src/main/java/io/github/ololx/moonshine/bytes/ByteIndexOperator.java
+++ moonshine-bytes/src/main/java/io/github/ololx/moonshine/bytes/ByteIndexOperator.java
@@ -18,7 +18,16 @@
 package io.github.ololx.moonshine.bytes;
 
 /**
+ * A functional interface for applying a mapping function to a byte index.
  *
+ * @apiNote
+ * This interface is designed to be used with indexed byte arrays, where each
+ * element of the array is a single byte and the index represents the position
+ * of that byte in the array.
+ *
+ * @implSpec
+ * The default implementation of {@link #identity()} returns a lambda that
+ * simply returns its input, effectively mapping each index to itself.
  *
  * project moonshine
  * created 02.03.2023 10:22
@@ -27,9 +36,25 @@ package io.github.ololx.moonshine.bytes;
  */
 public interface ByteIndexOperator {
 
+    /**
+     * Applies a mapping function to the given byte index.
+     *
+     * @param index the byte index to apply the mapping function to
+     * @return the result of applying the mapping function to the byte index
+     */
     int apply(int index);
 
+    /**
+     * Returns a byte index operator that applies the identity function.
+     *
+     * @implSpec
+     * The default implementation returns a lambda that simply returns its
+     * input, effectively mapping each index to itself.
+     *
+     * @return a byte index operator that applies the identity function
+     */
     static ByteIndexOperator identity() {
-        return t -> t;
+        return i -> i;
     }
 }
+
