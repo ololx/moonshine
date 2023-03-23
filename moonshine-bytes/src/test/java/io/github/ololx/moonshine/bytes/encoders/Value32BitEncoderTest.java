c6a4e7d337e6662c2af4635b64e1337aa182a53e
a-kropotin <a.kropotin@pixonic.com>
2023-03-23 13:16:21 +0200

    chore(*) add tets for 8-16 bit decoders

--- moonshine-bytes/src/test/java/io/github/ololx/moonshine/bytes/encoders/Value32BitEncoderTest.java
+++ moonshine-bytes/src/test/java/io/github/ololx/moonshine/bytes/encoders/Value32BitEncoderTest.java
@@ -15,8 +15,9 @@
  * limitations under the License.
  */
 
-package io.github.ololx.moonshine.bytes;
+package io.github.ololx.moonshine.bytes.encoders;
 
+import io.github.ololx.moonshine.bytes.Endianness;
 import io.github.ololx.moonshine.bytes.coding.ByteIndexOperator;
 import io.github.ololx.moonshine.bytes.coding.encoders.ValueBytesEncoder;
 import org.testng.annotations.DataProvider;
