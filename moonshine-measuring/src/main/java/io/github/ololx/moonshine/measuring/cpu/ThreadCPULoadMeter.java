aef7d9f91dfe46e011facb33d67370847ba72d05
a-kropotin <a.kropotin@pixonic.com>
2023-04-13 21:05:41 +0300

    add new meters

---
+++ moonshine-measuring/src/main/java/io/github/ololx/moonshine/measuring/cpu/ThreadCPULoadMeter.java
@@ -0,0 +1,121 @@
+/**
+ * Copyright 2023 the project moonshine authors
+ * and the original author or authors annotated by {@author}
+ * <br/>
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * <br/>
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * <br/>
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package io.github.ololx.moonshine.measuring.cpu;
+
+import com.sun.management.OperatingSystemMXBean;
+import io.github.ololx.moonshine.measuring.Measurer;
+
+import java.lang.management.ManagementFactory;
+import java.time.Duration;
+import java.util.Objects;
+
+/**
+ * project moonshine
+ * created 13.04.2023 14:29
+ *
+ * @author Alexander A. Kropotin
+ */
+public class ThreadCPULoadMeter implements Measurer<Double> {
+
+    /**
+     * The OperatingSystemMXBean instance used to obtain CPU load information.
+     */
+    private final OperatingSystemMXBean osBean;
+
+    /**
+     * The CPU load at the start of the measurement period.
+     */
+    private double startCpuLoad;
+    private long startCpuLoadT;
+
+    /**
+     * The CPU load at the end of the measurement period.
+     */
+    private double endCpuLoad;
+    private long endCpuLoadT;
+
+    /**
+     * Creates a new instance of the ThreadCPULoadMeter class.
+     *
+     * <p>This constructor uses the default OperatingSystemMXBean instance obtained from
+     * the {@link ManagementFactory#getOperatingSystemMXBean()} method.</p>
+     */
+    public ThreadCPULoadMeter() {
+        this.osBean = (OperatingSystemMXBean) ManagementFactory.getOperatingSystemMXBean();
+    }
+
+    /**
+     * Creates a new instance of the ThreadCPULoadMeter class using
+     * the specified OperatingSystemMXBean instance.
+     *
+     * @param osBean the OperatingSystemMXBean instance to use
+     * @throws NullPointerException if osBean is null
+     */
+    ThreadCPULoadMeter(OperatingSystemMXBean osBean) {
+        this.osBean = Objects.requireNonNull(
+                osBean,
+                "The OS MX bean must be not null"
+        );
+    }
+
+    /**
+     * Marks the beginning of the measurement period.
+     *
+     * <p>This method should be called before the code whose CPU load
+     * is to be measured.</p>
+     *
+     * @return this ThreadCPULoadMeter instance
+     */
+    @Override
+    public ThreadCPULoadMeter start() {
+        this.startCpuLoad = this.osBean.getProcessCpuLoad();
+        this.startCpuLoadT = System.nanoTime();
+
+
+        return this;
+    }
+
+    /**
+     * Marks the end of the measurement period.
+     *
+     * <p>This method should be called after the code whose CPU load
+     * is to be measured.</p>
+     *
+     * @return this ThreadCPULoadMeter instance
+     */
+    @Override
+    public ThreadCPULoadMeter stop() {
+        this.endCpuLoad = this.osBean.getProcessCpuLoad();
+        this.endCpuLoadT = System.nanoTime();
+
+        return this;
+    }
+
+    /**
+     * Returns the CPU load during the measurement period, as a percentage.
+     *
+     * <p>This method should be called after {@link #stop()} method.</p>
+     *
+     * @return a Double object representing the CPU load during the measurement period, as a percentage
+     */
+    @Override
+    public Double result() {
+        Duration tb = Duration.ofNanos(this.endCpuLoadT).minusNanos(this.startCpuLoadT);
+        return tb.getNano() > 0 ? (this.endCpuLoad - this.startCpuLoad) * 100 * Runtime.getRuntime().availableProcessors(): 0D;
+    }
+}
---
+++ moonshine-measuring/src/test/java/io/github/ololx/moonshine/measuring/memory/ThreadGroupMemoryAllocationMeterTest.java
@@ -0,0 +1,184 @@
+/**
+ * Copyright 2023 the project moonshine authors
+ * and the original author or authors annotated by {@author}
+ * <br/>
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * <br/>
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * <br/>
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package io.github.ololx.moonshine.measuring.memory;
+
+import io.github.ololx.moonshine.measuring.cpu.CPULoadMeasurer;
+import io.github.ololx.moonshine.measuring.cpu.ThreadCPULoadMeter;
+import org.testng.annotations.Test;
+
+import java.util.concurrent.CompletableFuture;
+
+import static org.testng.Assert.assertEquals;
+import static org.testng.Assert.assertTrue;
+
+/**
+ * project moonshine
+ * created 07.04.2023 21:06
+ *
+ * @author Alexander A. Kropotin
+ */
+public class ThreadGroupMemoryAllocationMeterTest {
+
+    @Test
+    public void startAndStopAndResult_whenMeasurerWasActivated_thenReturnMeasuringResult() {
+        //Given
+        // the memory meter
+
+        CompletableFuture.allOf(
+                CompletableFuture.runAsync(() -> {
+                    CPULoadMeasurer meter = new CPULoadMeasurer(Thread.currentThread().getId());
+
+                    meter.start();
+                    //When
+                    // start measurer
+
+                    // create new array with 1_000_000 int
+                    int[] array = new int[9_000_000];
+                    for (int i = 0; i < array.length; i++) {
+                        array[i] = i;
+                    }
+                    // stop measurer
+                    meter.stop();
+
+                    System.out.println(meter.result());
+                }),
+                CompletableFuture.runAsync(() -> {
+                    CPULoadMeasurer meter = new CPULoadMeasurer(Thread.currentThread().getId());
+
+                    meter.start();
+                    //When
+                    // start measurer
+
+                    // create new array with 1_000_000 int
+                    int[] array = new int[9_000_000];
+                    for (int i = 0; i < array.length; i++) {
+                        array[i] = i;
+                    }
+                    // stop measurer
+                    meter.stop();
+
+                    System.out.println(meter.result());
+                }),
+                CompletableFuture.runAsync(() -> {
+                    CPULoadMeasurer meter = new CPULoadMeasurer(Thread.currentThread().getId());
+
+                    meter.start();
+                    //When
+                    // start measurer
+
+                    // create new array with 1_000_000 int
+                    int[] array = new int[9_000_000];
+                    for (int i = 0; i < array.length; i++) {
+                        array[i] = i;
+                    }
+                    // stop measurer
+                    meter.stop();
+
+                    System.out.println(meter.result());
+                })
+        ).join();
+        CompletableFuture.runAsync(() -> {
+            CPULoadMeasurer meter = new CPULoadMeasurer(Thread.currentThread().getId());
+
+            meter.start();
+            //When
+            // start measurer
+
+            // create new array with 1_000_000 int
+            int[] array = new int[9_000_000];
+            for (int i = 0; i < array.length; i++) {
+                array[i] = i;
+            }
+            // stop measurer
+            meter.stop();
+
+            System.out.println(meter.result());
+        }).join();
+
+        //Then
+        // allocated memory is more than 0
+        //assertEquals(meter.result().toBytes(), 0);
+    }
+
+    @Test
+    public void start2AndStopAndResult_whenMeasurerWasActivated_thenReturnMeasuringResult() {
+        //Given
+        // the memory meter
+        CPULoadMeasurer meter = new CPULoadMeasurer(Thread.currentThread().getId());
+
+
+        ThreadCPULoadMeter meters = new ThreadCPULoadMeter();
+        meters.start();
+        meter.start();
+        //When
+        // start measurer
+
+        // create new array with 1_000_000 int
+        int numCore = Runtime.getRuntime().availableProcessors();
+        int numThreadsPerCore = 2;
+        double load = 1;
+        final long duration = 999999999;
+        for (int thread = 0; thread < numCore * numThreadsPerCore; thread++) {
+            new BusyThread("Thread" + thread, load, duration).start();
+        }
+        // stop measurer
+        meters.stop();
+        meter.stop();
+        System.out.println(meter.result());
+        System.out.println(" | " + meters.result());
+    }
+
+    private static class BusyThread extends Thread {
+        private double load;
+        private long duration;
+
+        /**
+         * Constructor which creates the thread
+         * @param name Name of this thread
+         * @param load Load % that this thread should generate
+         * @param duration Duration that this thread should generate the load for
+         */
+        public BusyThread(String name, double load, long duration) {
+            super(name);
+            this.load = load;
+            this.duration = duration;
+        }
+
+        /**
+         * Generates the load when run
+         */
+        @Override
+        public void run() {
+
+            long startTime = System.currentTimeMillis();
+            try {
+                // Loop for the given duration
+                while (System.currentTimeMillis() - startTime < duration) {
+                    // Every 100ms, sleep for the percentage of unladen time
+                    if (System.currentTimeMillis() % 100 == 0) {
+                        Thread.sleep((long) Math.floor((1 - load) * 100));
+                    }
+                }
+
+
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+}
