0ab83ced8a82522534d6c8994d735dcc5c16717a
a-kropotin <a.kropotin@pixonic.com>
2023-06-19 11:13:56 +0300

    feature(*) add string decoder/encoder

---
+++ moonshine-bytes/src/test/java/io/github/ololx/moonshine/bytes/coding/encoders/StringEncoderTest.java
@@ -0,0 +1,57 @@
+/**
+ * Copyright 2023 the project moonshine authors
+ * and the original author or authors annotated by {@author}
+ * <br/>
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * <br/>
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * <br/>
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package io.github.ololx.moonshine.bytes.coding.encoders;
+
+import io.github.ololx.moonshine.bytes.coding.ByteIndexOperator;
+import io.github.ololx.moonshine.bytes.coding.decoders.StringDecoder;
+import org.testng.annotations.Test;
+
+import java.time.ZonedDateTime;
+import java.util.Arrays;
+
+import static org.testng.Assert.assertEquals;
+
+/**
+ * project moonshine
+ * created 19.06.2023 11:10
+ *
+ * @author Alexander A. Kropotin
+ */
+public class StringEncoderTest {
+
+    @Test
+    public void encode_whenEncodeValue_thenEncodedBytesEqualsExpectedBytes() {
+        //Given
+        // byte encoder and origin value
+        StringEncoder encoder = new StringEncoder();
+        StringDecoder decoder = new StringDecoder();
+
+        String str = "foo-bar";
+        //When
+        // encode value
+        byte[] encodedValue = encoder.encode(str, ByteIndexOperator.identity());
+        String decodedValue = decoder.decode(encodedValue, ByteIndexOperator.identity());
+
+        System.out.println(Arrays.toString(encodedValue));
+        System.out.println(decodedValue);
+
+        //Then
+        // encoded value equals expected bytes
+        assertEquals(str, decodedValue);
+    }
+}
