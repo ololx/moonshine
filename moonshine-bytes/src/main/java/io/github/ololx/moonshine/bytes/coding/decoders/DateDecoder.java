6a2bcb417b1962bd13f02ef2272d1026062976a5
ololx <ololx@icloud.com>
2023-06-20 08:14:47 +0300

    chore(*) add date encoder/decoder

---
+++ moonshine-bytes/src/main/java/io/github/ololx/moonshine/bytes/coding/decoders/DateDecoder.java
@@ -0,0 +1,53 @@
+/**
+ * Copyright 2023 the project moonshine authors
+ * and the original author or authors annotated by {@author}
+ * <br/>
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * <br/>
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * <br/>
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package io.github.ololx.moonshine.bytes.coding.decoders;
+
+import io.github.ololx.moonshine.bytes.coding.ByteIndexOperator;
+
+import java.time.Instant;
+import java.util.Date;
+
+/**
+ * The decoder that converts byte arrays to a {@code Byte} value.
+ *
+ * project moonshine
+ * created 20.06.2023 08:11
+ *
+ * @author Alexander A. Kropotin
+ */
+public class DateDecoder implements ValueBytesDecoder<Date> {
+
+    /**
+     * Decodes a byte array to a {@code Date} value starting at a given offset
+     * with specified endianness.
+     *
+     * @param bytes the byte array to decode
+     * @param offset the offset at which decoding should start
+     * @param endianness the endianness to be used for encoding
+     * @return the decoded value of given type
+     * @throws IndexOutOfBoundsException if {@code offset + endianness.appy(i)}
+     * is out of the {@code bytes} bounds
+     */
+    @Override
+    public Date decode(byte[] bytes, int offset, ByteIndexOperator endianness) {
+        return Date.from(Instant.ofEpochMilli(
+                ValueBytesDecoder.value64BitDecoder()
+                        .decode(bytes, offset, endianness)
+        ));
+    }
+}
---
+++ moonshine-bytes/src/test/java/io/github/ololx/moonshine/bytes/coding/encoders/DateEncoderTest.java
@@ -0,0 +1,60 @@
+/**
+ * Copyright 2023 the project moonshine authors
+ * and the original author or authors annotated by {@author}
+ * <br/>
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * <br/>
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * <br/>
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package io.github.ololx.moonshine.bytes.coding.encoders;
+
+import io.github.ololx.moonshine.bytes.coding.ByteIndexOperator;
+import io.github.ololx.moonshine.bytes.coding.decoders.DateDecoder;
+import io.github.ololx.moonshine.bytes.coding.decoders.LocalDateDecoder;
+import org.testng.annotations.Test;
+
+import java.time.Instant;
+import java.time.LocalDate;
+import java.util.Arrays;
+import java.util.Date;
+
+import static org.testng.Assert.assertEquals;
+
+/**
+ * project moonshine
+ * created 25.03.2023 18:28
+ *
+ * @author Alexander A. Kropotin
+ */
+public class DateEncoderTest {
+
+    @Test
+    public void encode_whenEncodeValue_thenEncodedBytesEqualsExpectedBytes() {
+        //Given
+        // byte encoder and origin value
+        DateEncoder encoder = new DateEncoder();
+        DateDecoder decoder = new DateDecoder();
+
+        //When
+        // encode value
+        Date nowDate = Date.from(Instant.now());
+        byte[] encodedValue = encoder.encode(nowDate, ByteIndexOperator.identity());
+        Date decodedValue = decoder.decode(encodedValue, ByteIndexOperator.identity());
+
+        System.out.println(Arrays.toString(encodedValue));
+        System.out.println(decodedValue);
+
+        //Then
+        // encoded value equals expected bytes
+        assertEquals(nowDate, decodedValue);
+    }
+}
