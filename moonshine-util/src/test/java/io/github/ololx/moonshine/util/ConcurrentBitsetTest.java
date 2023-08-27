bf1da96323241c0db21007c8f55b8f420e293d69
ololx <ololx@icloud.com>
2023-08-27 22:40:41 +0300

    chore(ConcurrentBitsetTest.java) add invoc count to tests

--- moonshine-util/src/test/java/io/github/ololx/moonshine/util/ConcurrentBitsetTest.java
+++ moonshine-util/src/test/java/io/github/ololx/moonshine/util/ConcurrentBitsetTest.java
@@ -19,8 +19,8 @@ public class ConcurrentBitsetTest {
         List<Object[]> data = new ArrayList<>();
         Random random = new Random();
 
-        int bitSetSize = random.nextInt(1_000) + 100;
-        List<Integer> unitIndexes = IntStream.range(0, random.nextInt(99))
+        int bitSetSize = random.nextInt(100) + 100;
+        List<Integer> unitIndexes = IntStream.range(0, random.nextInt(50))
                 .boxed()
                 .filter(index -> random.nextBoolean())
                 .collect(Collectors.toList());
@@ -39,6 +39,11 @@ public class ConcurrentBitsetTest {
                 new ConcurrentBitSetWithSegmentsRWLocks(bitSetSize),
                 unitIndexes,
                 bitSetSize
+        });
+        data.add(new Object[]{
+            new ConcurrentBitSetWithSegmentsSynchronization(bitSetSize),
+            unitIndexes,
+            bitSetSize
         });
         data.add(new Object[]{
                 new NonBlockingConcurrentBitSet(bitSetSize),
@@ -68,7 +73,7 @@ public class ConcurrentBitsetTest {
                            .noneMatch(bit -> bit));
     }
 
-    @Test(dataProvider = "bitsetData")
+    @Test(dataProvider = "bitsetData", invocationCount = 30)
     public void set_whenSetBit_thenBitIsTrue(ConcurrentBitSet bitset, List<Integer> unitIndexes, int bitSetSize) {
         unitIndexes.parallelStream()
                 .forEach(bitset::set);
@@ -110,18 +115,14 @@ public class ConcurrentBitsetTest {
                            .noneMatch(bit -> bit));
     }
 
-    @Test(dataProvider = "bitsetData")
+    @Test(dataProvider = "bitsetData", invocationCount = 30)
     public void flip_whenBitWasTrue_thenBitIsFalseNowAndViceVersa(ConcurrentBitSet bitset, List<Integer> unitIndexes, int bitSetSize) {
         unitIndexes.parallelStream()
-                .forEach(bitset::set);
-        IntStream.range(0, bitSetSize)
-                .parallel()
                 .forEach(bitset::flip);
-
-        List<Boolean> nilBits = unitIndexes.stream()
+        List<Boolean> unitBits = unitIndexes.stream()
                 .map(bitset::get)
                 .collect(Collectors.toList());
-        List<Boolean> unitBits = IntStream.range(0, bitSetSize)
+        List<Boolean> nilBits = IntStream.range(0, bitSetSize)
                 .parallel()
                 .filter(index -> !unitIndexes.contains(index))
                 .mapToObj(bitset::get)
