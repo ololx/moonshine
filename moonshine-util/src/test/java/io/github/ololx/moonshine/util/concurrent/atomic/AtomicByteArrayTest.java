4c813b3c2ca35a3403a35014c746292ca7fcbf6e
a-kropotin <a.kropotin@pixonic.com>
2023-09-15 09:14:12 +0300

    chore(*) sync jacoco and tests versions

--- moonshine-util/src/test/java/io/github/ololx/moonshine/util/concurrent/atomic/AtomicByteArrayTest.java
+++ moonshine-util/src/test/java/io/github/ololx/moonshine/util/concurrent/atomic/AtomicByteArrayTest.java
@@ -1,5 +1,6 @@
 package io.github.ololx.moonshine.util.concurrent.atomic;
 
+import org.testng.annotations.BeforeClass;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 
@@ -13,25 +14,21 @@ import static org.testng.Assert.assertTrue;
  */
 public class AtomicByteArrayTest {
 
-    AtomicByteArray atomicByteArray;
-
-    @BeforeMethod
-    public void setUp() {
-        atomicByteArray = new AtomicByteArray(new byte[]{1, 2, 3, 4, 5});
-    }
-
     @Test
     public void length_whenGetLength_thenReturnCorrectLength() {
+        AtomicByteArray atomicByteArray = new AtomicByteArray(new byte[]{1, 2, 3, 4, 5});
         assertEquals(5, atomicByteArray.length());
     }
 
     @Test
     public void get_whenGet_thenCorrectValueReturned() {
+        AtomicByteArray atomicByteArray = new AtomicByteArray(new byte[]{1, 2, 3, 4, 5});
         assertEquals(1, atomicByteArray.get(0));
     }
 
     @Test
     public void set_whenSet_thenValueSetCorrectly() {
+        AtomicByteArray atomicByteArray = new AtomicByteArray(new byte[]{1, 2, 3, 4, 5});
         atomicByteArray.set(0, (byte) 9);
 
         assertEquals(9, atomicByteArray.get(0));
@@ -39,6 +36,7 @@ public class AtomicByteArrayTest {
 
     @Test
     public void getAndSet_whenSet_thenOldValueReturnedAndNewValueSet() {
+        AtomicByteArray atomicByteArray = new AtomicByteArray(new byte[]{1, 2, 3, 4, 5});
         byte oldValue = atomicByteArray.getAndSet(0, (byte) 7);
 
         assertEquals(1, oldValue);
@@ -47,12 +45,15 @@ public class AtomicByteArrayTest {
 
     @Test
     public void compareAndSet_whenExpectMatches_thenValueSetAndReturnTrue() {
+        AtomicByteArray atomicByteArray = new AtomicByteArray(new byte[]{1, 2, 3, 4, 5});
+
         assertTrue(atomicByteArray.compareAndSet(0, (byte) 1, (byte) 8));
         assertEquals(8, atomicByteArray.get(0));
     }
 
     @Test
     public void getAndIncrement_whenIncrement_thenIncrementValueAndReturnOldValue() {
+        AtomicByteArray atomicByteArray = new AtomicByteArray(new byte[]{1, 2, 3, 4, 5});
         byte oldValue = atomicByteArray.getAndIncrement(0);
 
         assertEquals(1, oldValue);
@@ -61,6 +62,7 @@ public class AtomicByteArrayTest {
 
     @Test
     public void getAndDecrement_whenDecrement_thenDecrementValueAndReturnOldValue() {
+        AtomicByteArray atomicByteArray = new AtomicByteArray(new byte[]{1, 2, 3, 4, 5});
         byte oldValue = atomicByteArray.getAndDecrement(1);
 
         assertEquals(2, oldValue);
@@ -69,6 +71,7 @@ public class AtomicByteArrayTest {
 
     @Test
     public void incrementAndGet_whenIncrement_thenIncrementValueAndReturnNewValue() {
+        AtomicByteArray atomicByteArray = new AtomicByteArray(new byte[]{1, 2, 3, 4, 5});
         byte newValue = atomicByteArray.incrementAndGet(0);
 
         assertEquals(2, newValue);
@@ -76,6 +79,7 @@ public class AtomicByteArrayTest {
 
     @Test
     public void decrementAndGet_whenDecrement_thenDecrementValueAndReturnNewValue() {
+        AtomicByteArray atomicByteArray = new AtomicByteArray(new byte[]{1, 2, 3, 4, 5});
         byte newValue = atomicByteArray.decrementAndGet(1);
 
         assertEquals(1, newValue);
@@ -83,6 +87,7 @@ public class AtomicByteArrayTest {
 
     @Test
     public void addAndGet_whenAdd_thenAddValueAndReturnNewValue() {
+        AtomicByteArray atomicByteArray = new AtomicByteArray(new byte[]{1, 2, 3, 4, 5});
         byte newValue = atomicByteArray.addAndGet(1, (byte) 3);
 
         assertEquals(5, newValue);
@@ -90,6 +95,7 @@ public class AtomicByteArrayTest {
 
     @Test
     public void getAndAdd_whenAdd_thenAddValueAndReturnOldValue() {
+        AtomicByteArray atomicByteArray = new AtomicByteArray(new byte[]{1, 2, 3, 4, 5});
         byte oldValue = atomicByteArray.getAndAdd(1, (byte) 2);
 
         assertEquals(2, oldValue);
@@ -98,6 +104,7 @@ public class AtomicByteArrayTest {
 
     @Test
     public void getAndUpdate_whenUpdate_thenUpdateValueAndReturnOldValue() {
+        AtomicByteArray atomicByteArray = new AtomicByteArray(new byte[]{1, 2, 3, 4, 5});
         byte oldValue = atomicByteArray.getAndUpdate(0, x -> (byte) (x + 5));
 
         assertEquals(1, oldValue);
@@ -106,6 +113,7 @@ public class AtomicByteArrayTest {
 
     @Test
     public void updateAndGet_whenUpdate_thenUpdateValueAndReturnNewValue() {
+        AtomicByteArray atomicByteArray = new AtomicByteArray(new byte[]{1, 2, 3, 4, 5});
         byte newValue = atomicByteArray.updateAndGet(0, x -> (byte) (x + 5));
 
         assertEquals(6, newValue);
@@ -113,6 +121,7 @@ public class AtomicByteArrayTest {
 
     @Test
     public void getAndAccumulate_whenAccumulate_thenAccumulateValueAndReturnOldValue() {
+        AtomicByteArray atomicByteArray = new AtomicByteArray(new byte[]{1, 2, 3, 4, 5});
         byte oldValue = atomicByteArray.getAndAccumulate(0, (byte) 3, (x, y) -> (byte) (x * y));
 
         assertEquals(1, oldValue);
@@ -121,6 +130,7 @@ public class AtomicByteArrayTest {
 
     @Test
     public void accumulateAndGet_whenAccumulate_thenAccumulateValueAndReturnNewValue() {
+        AtomicByteArray atomicByteArray = new AtomicByteArray(new byte[]{1, 2, 3, 4, 5});
         byte newValue = atomicByteArray.accumulateAndGet(0, (byte) 3, (x, y) -> (byte) (x * y));
 
         assertEquals(3, newValue);
@@ -128,6 +138,7 @@ public class AtomicByteArrayTest {
 
     @Test
     public void toString_whenGetString_thenReturnCorrectStringRepresentation() {
+        AtomicByteArray atomicByteArray = new AtomicByteArray(new byte[]{1, 2, 3, 4, 5});
         assertEquals("[1, 2, 3, 4, 5]", atomicByteArray.toString());
     }
 }
